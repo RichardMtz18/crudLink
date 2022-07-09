7dcc11b639dfe1bceaa124db86c1805830b780c4
RichardMtz18 <utm20040085@utma.edu.mx>
2022-07-08 20:40:43 -0500

    Projecto 2 parcial - Landing Page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CrudComponent } from './pages/crud/crud.component';
+import { LandingPageComponent } from './pages/landing-page/landing-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "crud", component: CrudComponent},
+  {path: "", component: LandingPageComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CrudComponent } from './pages/crud/crud.component';
+import { LandingPageComponent } from './pages/landing-page/landing-page.component';
+import { RegistrarUsuarioComponent } from './pages/crud/registrar-usuario/registrar-usuario.component';
+import { ActualizarUsuarioComponent } from './pages/crud/actualizar-usuario/actualizar-usuario.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CrudComponent,
+    LandingPageComponent,
+    RegistrarUsuarioComponent,
+    ActualizarUsuarioComponent
   ],
   imports: [
     BrowserModule,
