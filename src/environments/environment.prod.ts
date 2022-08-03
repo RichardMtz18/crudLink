082adbd87e4223bac9535eeebf58455c44b5a9ee
RichardMtz18 <utm20040085@utma.edu.mx>
2022-08-02 22:09:13 -0500

    commit proyecto

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  baseUrl: 'https://api-tidsm-6a.herokuapp.com/api',
+  baseUrl: 'https://api-tidsm-6a.herokuapp.com/api/empresa',
   matricula: 'UTM20040085'
 };
